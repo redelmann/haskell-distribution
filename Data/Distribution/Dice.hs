fed06b5d03f432cd1005ede212a2f775eddb5ddc
Romain Edelmann <romain.edelmann@gmail.com>
2014-07-28 21:25:11 +0200

    Added type synomym for dice.

--- Data/Distribution/Dice.hs
+++ Data/Distribution/Dice.hs
@@ -4,7 +4,8 @@
 --   functions on dice.
 module Data.Distribution.Dice
     ( -- * Dice
-      dice
+      Dice
+    , dice
       -- ** Common dice
     , d4
     , d6
@@ -17,38 +18,41 @@ module Data.Distribution.Dice
 
 import Data.Distribution
 
+-- | Distribution of the result of dice rolls.
+type Dice = Distribution Int
+
 -- | Fair dice of @n@ faces.
-dice :: Int -> Distribution Int
+dice :: Int -> Dice
 dice n = uniform [1 .. n]
 
 -- | Fair dice of @4@ faces.
-d4 :: Distribution Int
+d4 :: Dice
 d4 = dice 4
 
 -- | Fair dice of @6@ faces.
-d6 :: Distribution Int
+d6 :: Dice
 d6 = dice 6
 
 -- | Fair dice of @8@ faces.
-d8 :: Distribution Int
+d8 :: Dice
 d8 = dice 8
 
 -- | Fair dice of @10@ faces.
-d10 :: Distribution Int
+d10 :: Dice
 d10 = dice 10
 
 -- | Fair dice of @12@ faces.
-d12 :: Distribution Int
+d12 :: Dice
 d12 = dice 12
 
 -- | Fair dice of @20@ faces.
-d20 :: Distribution Int
+d20 :: Dice
 d20 = dice 20
 
 -- | Rolls `n` times the given dice and sums the results.
-rollsOf :: Int -> Distribution Int -> Distribution Int
+rollsOf :: Int -> Dice -> Dice
 n `rollsOf` d = n `times` d
 
 -- | Rerolls the dice once if the first outcome satifies the given predicate.
-rerollOn :: (Int -> Bool) -> Distribution Int -> Distribution Int
+rerollOn :: (Int -> Bool) -> Dice -> Dice
 rerollOn f d = d `andThen` \ n -> if f n then d else always n
